33dfd204d70d6dd08d64e5cb3bd20d647cb03663
savo asanin <savo.asanin@gmail.com>
2024-10-11 10:07:51 +0200

    Created separate file for tekonize function tokenize_mod

--- app/run.py
+++ app/run.py
@@ -14,53 +14,16 @@ from plotly.graph_objs import Bar
 from joblib import load
 from sqlalchemy import create_engine
 
+from tokenize_mod import tokenize_mod
 
 app = Flask(__name__)
 
-def tokenize(text):
-
-    """
-    Tokenizes the input text by removing URLs, stripping non-alphabetic characters, 
-    tokenizing the text into words, lemmatizing the words, and removing stop words.
-    
-    Args:
-        text (str): The input text to be tokenized.
-        
-    Returns:
-        list: The list of cleaned tokens.
-    """
-
-    url_regex = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-    rest_regex = r"[^a-zA-Z0-9]"
-    
-    #Stripping messages of all urls ?
-    detected_urls = re.findall(url_regex, text)
-    for url in detected_urls:
-        text = text.replace(url, "urlplaceholder")
-    
-    #Stripping the messages of all symbols like ., or ?
-    stripped = re.sub(rest_regex," ",text)
-    # Tokenize the sentences to words
-    tokens = word_tokenize(stripped)
-    
-    # Lemmatize the words (e.g. defined to define)
-    lemmatizer = WordNetLemmatizer()
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        
-        #Remove Stop-words
-        if tok not in stopwords.words("english"):
-            clean_tokens.append(clean_tok)
-        
-    return clean_tokens
-
 # load data
-engine = create_engine('sqlite:///data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('DisasterResponse.db', engine)
 
 # load model
-model = load("models/classifier.pkl")
+model = load("../models/classifier.pkl")
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
